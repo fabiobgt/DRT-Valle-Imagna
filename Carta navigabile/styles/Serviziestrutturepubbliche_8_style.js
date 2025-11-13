bfb727d3f0bb679b9f150cf1c444264e80f55000
CONCRETA SRL <fabio.bagattini@concreta.srl>
2025-11-13 16:48:52 +0100

    Add files via upload

---
+++ Carta navigabile/styles/Serviziestrutturepubbliche_8_style.js
@@ -0,0 +1,32 @@
+var size = 0;
+var placement = 'point';
+
+var style_Serviziestrutturepubbliche_8 = function(feature, resolution){
+    var context = {
+        feature: feature,
+        variables: {}
+    };
+    
+    var labelText = ""; 
+    var value = feature.get("");
+    var labelFont = "15.600000000000001px \'Gill Sans\', sans-serif";
+    var labelFill = "#323232";
+    var bufferColor = "#fafafa";
+    var bufferWidth = 1.5;
+    var textAlign = "left";
+    var offsetX = 0;
+    var offsetY = 0;
+    var placement = 'point';
+    if ("" !== null) {
+        labelText = String("");
+    }
+    var style = [ new ol.style.Style({
+        image: new ol.style.RegularShape({radius: 3.0 + size, points: 4,
+            angle: Math.PI/4, displacement: [0, 0], stroke: new ol.style.Stroke({color: 'rgba(0,0,0,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0.76}), fill: new ol.style.Fill({color: 'rgba(173,45,41,1.0)'})}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+
+    return style;
+};
